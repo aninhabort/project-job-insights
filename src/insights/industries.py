18baec1fb30b33df4507d3f0a87569b7113960d8
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2023-03-24 17:55:57 -0300

    Retirando comentarios em funcoes ja aprovadas

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -3,20 +3,6 @@ from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
     industry_file = read(path)
     all_indrustries = set()
 
